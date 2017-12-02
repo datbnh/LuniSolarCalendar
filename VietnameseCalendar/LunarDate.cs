6c1bd94aa57a8e3e3da74f2c63e2b2ba7669f8e8
datbnh <datbnh@gmail.com>
2017-12-03 02:33:07 +1100

    Clean up code.

--- VietnameseCalendar/LunarDate.cs
+++ VietnameseCalendar/LunarDate.cs
@@ -8,46 +8,48 @@ namespace Augustine.VietnameseCalendar
 {
 	public class LunarDate
 	{
-		public static string[] MonthNames = { "Giêng", "Hai", "Ba", "Tư",
-											"Năm", "Sáu", "Bảy", "Tám",
-											"Chín", "Mười", "Một (11)", "Chạp"};
-		public static string[] Stems = {"Giáp", "Ất", "Bính", "Đinh", "Mậu",
-										 "Kỷ", "Canh", "Tân", "Nhâm", "Quý"};
-		public static string[] Branches = {"Tý", "Sửu", "Dần", "Mão",
-											 "Thìn", "Tỵ", "Ngọ", "Mùi",
-											 "Thân", "Dậu", "Tuất", "Hợi"};
+        public static readonly string[] MonthNames = 
+            { "Giêng", "Hai", "Ba", "Tư", "Năm", "Sáu",
+              "Bảy", "Tám", "Chín", "Mười", "Một (11)", "Chạp"};
+		public static readonly string[] Stems = 
+            {"Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ", "Canh", "Tân", "Nhâm", "Quý"};
+		public static readonly string[] Branches = 
+            {"Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ",
+             "Ngọ", "Mùi", "Thân", "Dậu", "Tuất", "Hợi"};
 
-		public int Year;
-		public int Month;
-		public bool LeapMonth;
-		public int Day;
-        public double TimeZone;
+		public int Year { get; private set; }
+		public int Month { get; private set; }
+        public bool IsLeapMonth { get; private set; }
+        public int Day { get; private set; }
+        public double TimeZone { get; private set; }
 
-		public LunarDate(int year, int month, bool leapMonth, int day)
+        internal LunarDate(int year, int month, bool isLeapMonth, int day, double timeZone)
 		{
 			Year = year;
 			Month = month;
-			LeapMonth = leapMonth;
+			IsLeapMonth = isLeapMonth;
 			Day = day;
+            TimeZone = timeZone;
 		}
-
-
-        // Not using year cache
-        public static LunarDate SolarToLunar(int year, int month, int day, double timeZone)
+        
+        public static LunarDate FromSolar(int year, int month, int day, double timeZone)
         {
-            // case: 24/05/2000 (Solar) --- 21/04/2000 (Lunar)
+            // Sample case: Solar date 24/05/2000 --- Lunar date 21/04/2000
+            // previousNewMoon = 04/05/2000
+            // lunarYear = previousNewMoon.Year = 2000
+            // newMoon11Before = 08/12/1999
+            // newMoon11After = 26/11/2000
+            // (04/05/2000 - 08/12/1999) / 29 = 148 days / 29 = 5.10x
 
             int lunarYear;
             int lunarMonth;
             int lunarDay;
-            bool leapMonth = false;
+            bool isLeapMonth = false;
 
             var thisDay = new DateTime(year, month, day);
 
             var k = (int)((thisDay.AddHours(-timeZone).UniversalDateTimeToJulianDate() - 2415021.076998695)/ Astronomy.SynodicMonth + 0.5);
-            //Console.WriteLine((thisDay.AddHours(-timeZone).UniversalDateTimeToJulianDate() - 2415021.076998695) / Astronomy.SynodicMonth); // debug
 
-            // 04/05/2000
             var previousNewMoon =
                     Astronomy.JulianDateToUniversalDateTime(Astronomy.GetNewMoon(k)).AddHours(timeZone).Date;
             while (previousNewMoon > thisDay)
@@ -57,27 +59,25 @@ namespace Augustine.VietnameseCalendar
                 //Console.WriteLine("."); // debug
             } 
 
-            // 2000
             lunarYear = previousNewMoon.Year;
 
             // "previous, this/current" and "next" are not used to avoid ambiguity.
-            // newMoon11Before = 08/12/1999
-            // newMoon11After = 26/11/2000
             var newMoon11Before = Astronomy.NewMoon11(lunarYear - 1, timeZone).Date;
             var newMoon11After = Astronomy.NewMoon11(lunarYear, timeZone).Date;
 
             var isLeapYear = (newMoon11After - newMoon11Before).TotalDays > 365.0;
-
-            // 04/05/2000 - 08/12/1999 = 148 days
-            var daysFromNewMoon11BeforeToPreviousNewMoon = (previousNewMoon - newMoon11Before).TotalDays;
             
-            // = (int)(5.10) = 5
-            var monthsFromNewMoon11Before = (int) (daysFromNewMoon11BeforeToPreviousNewMoon / 29);
+            var monthsFromNewMoon11Before = (int) ((previousNewMoon - newMoon11Before).TotalDays / 29);
 
-            // monthsFromNewMoon11Before = 0: lunar month = 11, lunar year = previous year
-            // monthsFromNewMoon11Before = 1: lunar month = 12, lunar year = previous year
-            // monthsFromNewMoon11Before = 2: lunar month =  1, lunar year = this year
-            // monthsFromNewMoon11Before = 3: lunar month =  2, lunar year = this year
+            /* Note:
+             * monthsFromNewMoon11Before = 0: lunar month = 11, lunar year = previous year
+             * monthsFromNewMoon11Before = 1: lunar month = 12, lunar year = previous year
+             * monthsFromNewMoon11Before = 2: lunar month =  1, lunar year = this year
+             * monthsFromNewMoon11Before = 3: lunar month =  2, lunar year = this year
+             * and so on
+             */
+
+            //
             if (monthsFromNewMoon11Before < 2)
             {
                 lunarYear = lunarYear - 1;
@@ -88,6 +88,7 @@ namespace Augustine.VietnameseCalendar
                 lunarMonth = monthsFromNewMoon11Before - 1;
             }
 
+            // correcting month number if this lunar year is leap year
             if (isLeapYear)
             {
                 var leapMonthIndex = (new LunarYear(lunarYear, 7)).LeapMonthIndex;
@@ -96,75 +97,36 @@ namespace Augustine.VietnameseCalendar
                     lunarMonth--;
                     if (monthsFromNewMoon11Before == leapMonthIndex)
                     {
-                        leapMonth = true;
+                        isLeapMonth = true;
                     }
                 }
             }
             
-            // 
             lunarDay = (int)(thisDay - previousNewMoon).TotalDays + 1;
 
-            return new LunarDate(lunarYear, lunarMonth, leapMonth, lunarDay);
-            //var k, dayNumber, monthStart, a11, b11, lunarDay, lunarMonth, lunarYear, lunarLeap;
-            //dayNumber = jdFromDate(dd, mm, yy);
-            //k = INT((dayNumber - 2415021.076998695) / 29.530588853);
-            //monthStart = getNewMoonDay(k + 1, timeZone);
-            //if (monthStart > dayNumber)
-            //{
-            //    monthStart = getNewMoonDay(k, timeZone);
-            //}
-            //a11 = getLunarMonth11(yy, timeZone);
-            //b11 = a11;
-            //if (a11 >= monthStart)
-            //{
-            //    lunarYear = yy;
-            //    a11 = getLunarMonth11(yy - 1, timeZone);
-            //}
-            //else
-            //{
-            //    lunarYear = yy + 1;
-            //    b11 = getLunarMonth11(yy + 1, timeZone);
-            //}
-            //lunarDay = dayNumber - monthStart + 1;
-            //diff = INT((monthStart - a11) / 29);
-            //lunarLeap = 0;
-            //lunarMonth = diff + 11;
-            //if (b11 - a11 > 365)
-            //{
-            //    leapMonthDiff = getLeapMonthOffset(a11, timeZone);
-            //    if (diff >= leapMonthDiff)
-            //    {
-            //        lunarMonth = diff + 10;
-            //        if (diff == leapMonthDiff)
-            //        {
-            //            lunarLeap = 1;
-            //        }
-            //    }
-            //}
-            //if (lunarMonth > 12)
-            //{
-            //    lunarMonth = lunarMonth - 12;
-            //}
-            //if (lunarMonth >= 11 && diff < 4)
-            //{
-            //    lunarYear -= 1;
-            //}
+            return new LunarDate(lunarYear, lunarMonth, isLeapMonth, lunarDay, timeZone);
         }
-
-        public static int[] LunarToSolar(int lunarYear, int lunarMonth, int lunarDay, bool isLeapMonth, double timeZone)
+        
+        // overload method
+        public static LunarDate FromSolar(DateTime solarDate, double timeZone)
         {
-            return null;
+            return FromSolar(solarDate.Year, solarDate.Month, solarDate.Day, timeZone);
         }
 
+        public static DateTime ToSolar(int lunarYear, int lunarMonth, bool isLeapMonth, int lunarDay, double timeZone)
+        {
+            //int solarDay;
+            //int solarMonth;
+            //int solarYear;
+            throw new NotImplementedException();
+        }
 
-
-
-		// Year
-		/// <summary>
-		/// Get zero-based stem index (0: Giáp, 1: Ất...) of the current year.
-		/// </summary>
-		/// <returns></returns>
-		public int GetStemIndex()
+        #region Year
+        /// <summary>
+        /// Get zero-based stem index (0: Giáp, 1: Ất...) of the current year.
+        /// </summary>
+        /// <returns></returns>
+        public int GetStemIndex()
 		{
 			return (Year + 6) % 10;
 		}
@@ -177,23 +139,30 @@ namespace Augustine.VietnameseCalendar
 		{
 			return (Year + 8) % 12;
 		}
+
 		public string GetStemName()
 		{
 			return Stems[GetStemIndex()];
 		}
+
 		public string GetBranchName()
 		{
 			return Branches[GetBranchIndex()];
 		}
+
 		public string GetYearName()
 		{
 			return GetStemName() + " " + GetBranchName();
 		}
-		// Month
-		public int GetMonthStemIndex()
+
+        #endregion
+
+        #region Month
+        public int GetMonthStemIndex()
 		{
 			return (Year * 12 + Month + 3) % 10;
 		}
+
 		public int GetMonthBranchNumber()
 		{
 			switch (Month)
@@ -206,42 +175,42 @@ namespace Augustine.VietnameseCalendar
 					return Month + 1;
 			}
 		}
+
 		public string GetMonthStemName()
 		{
             if (Month < 0 || Month > MonthNames.Length)
                 return "Undefined";
             return Stems[GetMonthStemIndex()];
 		}
+
 		public string GetMonthBranchName()
 		{
             if (Month < 0 || Month > MonthNames.Length)
                 return "Undefined";
             return Branches[GetMonthBranchNumber()];
 		}
+
 		public string GetMonthFullName()
 		{
             if (Month < 0 || Month > MonthNames.Length)
                 return "Undefined";
             return GetMonthStemName() + " " + GetMonthBranchName();
 		}
+
 		public string GetMonthName()
 		{
             if (Month < 1 || Month > MonthNames.Length)
                 return "Undefined";
 			return MonthNames[Month - 1];
 		}
-		public string IsLeapString()
-		{
-			return (LeapMonth ? " (Nhuận)" : "");
-		}
+        #endregion
 
-		public override string ToString()
+        public override string ToString()
 		{
             return String.Format("({0:00}/{1:00}/{2:0000}) Ngày {3} tháng {4} ({5}){6} năm {7}",
                  Day, Month, Year,
-                 Day, GetMonthName(), GetMonthFullName(), 
-                 IsLeapString(), GetYearName());
+                 Day, GetMonthName(), GetMonthFullName(),
+                 IsLeapMonth ? " (Nhuận)" : "", GetYearName());
 		}
 	}
-
 }
