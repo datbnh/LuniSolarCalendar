f895d388b25023244e8d02fc2d6d600b666a7c49
datbnh <datbnh@gmail.com>
2017-12-06 23:14:24 +1100

    + Embed core dll in the final exe file (single file deploy).

--- VietnameseCalendarUI/App.xaml.cs
+++ VietnameseCalendarUI/App.xaml.cs
@@ -7,6 +7,9 @@
  *              https://github.com/datbnh/SolarLunarCalendar *
  *************************************************************/
 
+using System;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace Augustine.VietnameseCalendar.UI
@@ -16,5 +19,19 @@ namespace Augustine.VietnameseCalendar.UI
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(AssemblyResolver);
+        }
+
+        public Assembly AssemblyResolver(object sender, ResolveEventArgs args)
+        {
+            string dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
+            dllName = dllName.Replace(".", "_");
+            if (dllName.EndsWith("_resources")) return null;
+            System.Resources.ResourceManager rm = new System.Resources.ResourceManager(GetType().Namespace + ".Properties.Resources", System.Reflection.Assembly.GetExecutingAssembly());
+            byte[] bytes = (byte[])rm.GetObject(dllName);
+            return Assembly.Load(bytes);
+        }
     }
 }
--- VietnameseCalendarUI/Properties/Resources.Designer.cs
+++ VietnameseCalendarUI/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Augustine.VietnameseCalendar.UI.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Augustine_VietnameseCalendar_Core {
+            get {
+                object obj = ResourceManager.GetObject("Augustine_VietnameseCalendar_Core", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
