e760ad7b1c56565b30c4f7b0b2b97fa4e2a0e9e1
datbnh <datbnh@gmail.com>
2017-12-14 12:05:37 +1100

    + Improve code quality. + Improve sample window.

--- VietnameseCalendarUI/ThemeEditor.xaml.cs
+++ VietnameseCalendarUI/ThemeEditor.xaml.cs
@@ -29,65 +29,114 @@ namespace Augustine.VietnameseCalendar.UI
         {
             ExpanderColorsStackPanel.Children.Clear();
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.Background), nameof(ThemeColor.Foreground), "CHUNG", true));
-            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.NormalBackground), nameof(ThemeColor.Foreground), "Ngày Trong Tuần", false));
+            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.MouseOverBackground), null, "Ngày Đang Quan Tâm", false, "Ngày đang rê chuột lên."));
+            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.NormalBackground), null, "Ngày Trong Tuần", false));
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SaturdayBackground), nameof(ThemeColor.SaturdayForeground), "Thứ Bảy", false));
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SundayBackground), nameof(ThemeColor.SundayForeground), "Chủ Nhật", false));
-            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SpecialLevel1Background), nameof(ThemeColor.SpecialLevel1Foreground), "Ngày Đặc Biệt (Giáng Sinh, Tết...)", false));
-            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SpecialLevel2Background), nameof(ThemeColor.SpecialLevel2Foreground), "Ngày Đặc Biệt loại 2 (Rằm tháng Giêng, Tết Đoan Ngọ...)", false));
-            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SpecialLevel3Background), nameof(ThemeColor.SpecialLevel3Foreground), "Ngày Đặc Biệt loại 3 (nhắc nhở)", false));
+            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SpecialLevel1Background), nameof(ThemeColor.SpecialLevel1Foreground), "Ngày Đặc Biệt", false, "Những ngày như Tết, Giáng Sinh..."));
+            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SpecialLevel2Background), nameof(ThemeColor.SpecialLevel2Foreground), "Ngày Đặc Biệt loại 2", false, "Những ngày như Rằm tháng Giêng, Quốc Tế Phụ Nữ, Valentine..."));
+            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SpecialLevel3Background), nameof(ThemeColor.SpecialLevel3Foreground), "Ngày Đặc Biệt loại 3", false, "Những ngày mang tính nhắc nhở..."));
         }
 
-        private Expander CreateColorPickerPanel(string backgroundPath, string foregroundPath, string headerText, bool isExpanded = false)
+        private Expander CreateColorPickerPanel(string backgroundPath, string foregroundPath, string headerText, bool isExpanded = false, string toolTipText = "")
         {
-            var backgroundBinding =
-                new Binding(backgroundPath) { Mode = BindingMode.OneWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
-            var backgroundBindingTwoWay =
-                new Binding(backgroundPath) { Mode = BindingMode.TwoWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
+            // header
+            var header = new Grid()
+            {
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            header.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto, });
+            header.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto, });
 
-            var foregroundBinding =
-                new Binding(foregroundPath) { Mode = BindingMode.OneWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
-            var foregroundBindingTwoWay =
-                new Binding(foregroundPath) { Mode = BindingMode.TwoWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
+            var sampleText = new Label { Content = " Mẫu ", BorderBrush = Brushes.DimGray, BorderThickness = new Thickness(1), FontSize = 14 };
+            var sampleBlock = new Border() { Background = Resources["CrossBrush"] as LinearGradientBrush, Padding = new Thickness(3), BorderBrush = null, };
+            Grid.SetColumn(sampleBlock, 1);
 
-            var sampleText = new Run(" [ Mẫu ] ");
-            sampleText.SetBinding(Run.BackgroundProperty, backgroundBinding);
-            sampleText.SetBinding(Run.ForegroundProperty, foregroundBinding);
-            var headerTextBlock = new TextBlock();
-            headerTextBlock.Inlines.Add(new Run(headerText + " ") { FontWeight = FontWeights.Bold, });
-            headerTextBlock.Inlines.Add(sampleText);
+            if (backgroundPath != nameof(ThemeColor.Background)) {
+                var baseBackgroundBinding =
+                    new Binding(nameof(ThemeColor.Background))
+                    { Mode = BindingMode.OneWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
+                var sampleBaseBackground = new Border()
+                {
+                    Background = Resources["CrossBrush"] as LinearGradientBrush,
+                    Padding = new Thickness(3),
+                    BorderBrush = null,
+                };
+                sampleBaseBackground.SetBinding(Border.BackgroundProperty, baseBackgroundBinding);
+                sampleBaseBackground.Child = sampleText;
+                sampleBlock.Child = sampleBaseBackground;
+            } else
+            {
+                sampleBlock.Child = sampleText;
+            }
 
+            header.Children.Add(new Label() {
+                Content = headerText + " ",
+                FontWeight = FontWeights.Bold,
+                VerticalContentAlignment = VerticalAlignment.Center,
+                ToolTip = string.IsNullOrEmpty(toolTipText) ? null : toolTipText, });
+            header.Children.Add(sampleBlock);
+
+            // content
+            var mainStackPanel = new StackPanel();
+            var colorPickerStackPanel = new StackPanel() { Orientation = Orientation.Horizontal, };
             var colorInfoTextBlock = new TextBlock();
+            var backgroundColorPicker = new ColorPicker();
             var backgroundInfoString = new Run();
             var foregroundInfoString = new Run();
-            backgroundInfoString.SetBinding(Run.TextProperty, backgroundBinding);
-            foregroundInfoString.SetBinding(Run.TextProperty, foregroundBinding);
+                          
             colorInfoTextBlock.Inlines.Add("Màu nền: ");
             colorInfoTextBlock.Inlines.Add(backgroundInfoString);
             colorInfoTextBlock.Inlines.Add(" | Màu Chữ: ");
             colorInfoTextBlock.Inlines.Add(foregroundInfoString);
 
-            var backgroundColorPicker = new ColorPicker();
-            backgroundColorPicker.SetBinding(ColorPicker.BrushProperty, backgroundBindingTwoWay);
-            var foregroundColorPicker = new ColorPicker();
-            foregroundColorPicker.SetBinding(ColorPicker.BrushProperty, foregroundBindingTwoWay);
-
-            var colorPickerStackPanel = new StackPanel()
-            {
-                Orientation = Orientation.Horizontal,
-            };
             colorPickerStackPanel.Children.Add(backgroundColorPicker);
-            colorPickerStackPanel.Children.Add(foregroundColorPicker);
+            // foreground picker will be added in the below code if foregroundPath is not null nor empty
 
-            var mainStackPanel = new StackPanel();
             mainStackPanel.Children.Add(colorInfoTextBlock);
             mainStackPanel.Children.Add(colorPickerStackPanel);
 
+            // binding properties
+            var backgroundBinding =
+                new Binding(backgroundPath) { Mode = BindingMode.OneWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
+            var backgroundBindingTwoWay =
+                new Binding(backgroundPath) { Mode = BindingMode.TwoWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
+
+            sampleText.SetBinding(Label.BackgroundProperty, backgroundBinding);
+            backgroundInfoString.SetBinding(Run.TextProperty, backgroundBinding);
+            backgroundColorPicker.SetBinding(ColorPicker.BrushProperty, backgroundBindingTwoWay);
+
+            if (!string.IsNullOrEmpty(foregroundPath))
+            {
+                var foregroundBinding =
+                    new Binding(foregroundPath) { Mode = BindingMode.OneWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
+                var foregroundBindingTwoWay =
+                    new Binding(foregroundPath) { Mode = BindingMode.TwoWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
+
+                sampleText.SetBinding(Label.ForegroundProperty, foregroundBinding);
+                foregroundInfoString.SetBinding(Run.TextProperty, foregroundBinding);
+
+                var foregroundColorPicker = new ColorPicker();
+                foregroundColorPicker.SetBinding(ColorPicker.BrushProperty, foregroundBindingTwoWay);
+                colorPickerStackPanel.Children.Add(foregroundColorPicker);
+            }
+            else
+            {
+                var foregroundBinding =
+                    new Binding(nameof(ThemeColor.Foreground)) { Mode = BindingMode.OneWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
+
+                sampleText.SetBinding(Label.ForegroundProperty, foregroundBinding);
+                foregroundInfoString.Text = "(Theo màu chữ chung)";
+            }
+
+            // finally, the outmost UIElement
             var expander = new Expander()
             {
-                Header = headerTextBlock,
+                Header = header,
                 Content = mainStackPanel,
                 IsExpanded = isExpanded,
             };
+
             return expander;
         }
 
