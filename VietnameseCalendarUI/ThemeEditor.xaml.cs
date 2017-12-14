70608056fb876c1c35541cafe5610d28596f76b3
datbnh <datbnh@gmail.com>
2017-12-14 14:57:15 +1100

    + Display and two-way bind colors as hex strings to textboxes. Layout changed to alight the texts.

--- VietnameseCalendarUI/ThemeEditor.xaml.cs
+++ VietnameseCalendarUI/ThemeEditor.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 
 namespace Augustine.VietnameseCalendar.UI
 {
@@ -20,19 +21,21 @@ namespace Augustine.VietnameseCalendar.UI
         {
             InitializeComponent();
             DataContext = this;
-            ThemeColor1 = ThemeColors.DarkSemiTransparent;
-
-            
+            //ThemeColor1 = ThemeColors.DarkSemiTransparent;
         }
 
-        private void InitializeColorPickers()
+        private void SynchronizeColorPickers()
         {
             ExpanderColorsStackPanel.Children.Clear();
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.Background), nameof(ThemeColor.Foreground), "CHUNG", true));
+            ExpanderColorsStackPanel.Children.Add(new Separator());
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.MouseOverBackground), null, "Ngày Đang Quan Tâm", false, "Ngày đang rê chuột lên."));
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.NormalBackground), null, "Ngày Trong Tuần", false));
+            ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.GrayedOutBackground), nameof(ThemeColor.GrayedOutForeground), "Ngày Không Thuộc Tháng Hiện Tại", false));
+            ExpanderColorsStackPanel.Children.Add(new Separator());
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SaturdayBackground), nameof(ThemeColor.SaturdayForeground), "Thứ Bảy", false));
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SundayBackground), nameof(ThemeColor.SundayForeground), "Chủ Nhật", false));
+            ExpanderColorsStackPanel.Children.Add(new Separator());
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SpecialLevel1Background), nameof(ThemeColor.SpecialLevel1Foreground), "Ngày Đặc Biệt", false, "Những ngày như Tết, Giáng Sinh..."));
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SpecialLevel2Background), nameof(ThemeColor.SpecialLevel2Foreground), "Ngày Đặc Biệt loại 2", false, "Những ngày như Rằm tháng Giêng, Quốc Tế Phụ Nữ, Valentine..."));
             ExpanderColorsStackPanel.Children.Add(CreateColorPickerPanel(nameof(ThemeColor.SpecialLevel3Background), nameof(ThemeColor.SpecialLevel3Foreground), "Ngày Đặc Biệt loại 3", false, "Những ngày mang tính nhắc nhở..."));
@@ -79,21 +82,23 @@ namespace Augustine.VietnameseCalendar.UI
 
             // content
             var mainStackPanel = new StackPanel();
-            var colorPickerStackPanel = new StackPanel() { Orientation = Orientation.Horizontal, };
-            var colorInfoTextBlock = new TextBlock();
+            var colorPickerStackPanel = new StackPanel() { Orientation = Orientation.Horizontal, VerticalAlignment = VerticalAlignment.Center, };
+            //var colorInfoTextBlock = new TextBlock();
+            //var colorInfo = new Grid();
+            var colorInfoStackPanel = new StackPanel() { Orientation = Orientation.Horizontal, VerticalAlignment = VerticalAlignment.Center, };
             var backgroundColorPicker = new ColorPicker();
-            var backgroundInfoString = new Run();
-            var foregroundInfoString = new Run();
-                          
-            colorInfoTextBlock.Inlines.Add("Màu nền: ");
-            colorInfoTextBlock.Inlines.Add(backgroundInfoString);
-            colorInfoTextBlock.Inlines.Add(" | Màu Chữ: ");
-            colorInfoTextBlock.Inlines.Add(foregroundInfoString);
+            var backgroundInfoString = new TextBox() { VerticalContentAlignment = VerticalAlignment.Center, };
+            var foregroundInfoString = new TextBox() { VerticalContentAlignment = VerticalAlignment.Center, };
+
+            colorInfoStackPanel.Children.Add(new Label() { Content = "Màu nền: ", VerticalContentAlignment = VerticalAlignment.Center });
+            colorInfoStackPanel.Children.Add(backgroundInfoString);
+            colorInfoStackPanel.Children.Add(new Label() { Content = " | Màu Chữ: ", VerticalContentAlignment = VerticalAlignment.Center });
+            colorInfoStackPanel.Children.Add(foregroundInfoString);
 
             colorPickerStackPanel.Children.Add(backgroundColorPicker);
             // foreground picker will be added in the below code if foregroundPath is not null nor empty
 
-            mainStackPanel.Children.Add(colorInfoTextBlock);
+            mainStackPanel.Children.Add(colorInfoStackPanel);
             mainStackPanel.Children.Add(colorPickerStackPanel);
 
             // binding properties
@@ -103,7 +108,7 @@ namespace Augustine.VietnameseCalendar.UI
                 new Binding(backgroundPath) { Mode = BindingMode.TwoWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
 
             sampleText.SetBinding(Label.BackgroundProperty, backgroundBinding);
-            backgroundInfoString.SetBinding(Run.TextProperty, backgroundBinding);
+            backgroundInfoString.SetBinding(TextBox.TextProperty, backgroundBindingTwoWay);
             backgroundColorPicker.SetBinding(ColorPicker.BrushProperty, backgroundBindingTwoWay);
 
             if (!string.IsNullOrEmpty(foregroundPath))
@@ -114,7 +119,7 @@ namespace Augustine.VietnameseCalendar.UI
                     new Binding(foregroundPath) { Mode = BindingMode.TwoWay, Source = this.ThemeColor1, NotifyOnSourceUpdated = true, };
 
                 sampleText.SetBinding(Label.ForegroundProperty, foregroundBinding);
-                foregroundInfoString.SetBinding(Run.TextProperty, foregroundBinding);
+                foregroundInfoString.SetBinding(TextBox.TextProperty, foregroundBindingTwoWay);
 
                 var foregroundColorPicker = new ColorPicker();
                 foregroundColorPicker.SetBinding(ColorPicker.BrushProperty, foregroundBindingTwoWay);
@@ -127,6 +132,7 @@ namespace Augustine.VietnameseCalendar.UI
 
                 sampleText.SetBinding(Label.ForegroundProperty, foregroundBinding);
                 foregroundInfoString.Text = "(Theo màu chữ chung)";
+                foregroundInfoString.IsEnabled = false;
             }
 
             // finally, the outmost UIElement
@@ -143,9 +149,11 @@ namespace Augustine.VietnameseCalendar.UI
         public void SetTarget(AugustineCalendarMonth calendar)
         {
             target = calendar;
-            ThemeColor1 =  target.Theme.ThemeColor;
+            ThemeColor1 = (ThemeColor)target.Theme.ThemeColor.Clone();
             initialState = (ThemeColor)ThemeColor1.Clone();
-            //Resources["ThemeColor"] = ThemeColors.DarkSemiTransparent;// target.Theme.ThemeColor;
+
+            SynchronizeColorPickers();
+            SynchronizeAdvancedSettings();
         }
 
         public ThemeColor ThemeColor1 { get => (ThemeColor)GetValue(ThemeColor1Property); set => SetValue(ThemeColor1Property, value); }
@@ -192,12 +200,43 @@ namespace Augustine.VietnameseCalendar.UI
         private void apply_Click(object sender, RoutedEventArgs e)
         {
             initialState = (ThemeColor)ThemeColor1.Clone();
-            target.Theme = Themes.CreateTheme(ThemeColor1, null);
+            target.Theme = Theme.CreateTheme(ThemeColor1, null);
+            if ((bool)isDropShadow.IsChecked)
+            {
+                double r = 3;
+                double d = 1;
+                Color c = Colors.DimGray;
+
+                if (!double.TryParse(DropShadowRadius.Text, out r))
+                    DropShadowRadius.Text = r.ToString();
+                if (!double.TryParse(DropShadowDepth.Text, out d))
+                    DropShadowDepth.Text = d.ToString();
+                try { c = (Color)ColorConverter.ConvertFromString(DropShadowColor.Text); }
+                catch (Exception) { }
+
+                DropShadowColor.Text = c.ToString();
+
+                target.Effect = new DropShadowEffect { BlurRadius = r, Color = c, ShadowDepth = d };
+            } 
+            else
+            {
+                target.ClearValue(EffectProperty);
+            }
+
+            if (textFormattingMode.SelectedIndex == 0)
+            {
+                TextOptions.SetTextFormattingMode(target, TextFormattingMode.Ideal);
+            }
+            else if(textFormattingMode.SelectedIndex == 1)
+            {
+                TextOptions.SetTextFormattingMode(target, TextFormattingMode.Display);
+            }
         }
 
         private void window_Loaded(object sender, RoutedEventArgs e)
         {
-            InitializeColorPickers();
+            SynchronizeColorPickers();
+            SynchronizeAdvancedSettings();
         }
 
         private void cancel_Click(object sender, RoutedEventArgs e)
@@ -208,7 +247,28 @@ namespace Augustine.VietnameseCalendar.UI
         private void restore_Click(object sender, RoutedEventArgs e)
         {
             ThemeColor1 = (ThemeColor)initialState.Clone();
-            InitializeColorPickers();
+            SynchronizeColorPickers();
+            SynchronizeAdvancedSettings();
+        }
+
+        private void SynchronizeAdvancedSettings()
+        {
+            if (target.Effect != null && target.Effect is DropShadowEffect)
+            {
+                var ef = target.Effect as DropShadowEffect;
+                isDropShadow.IsChecked = true;
+                DropShadowRadius.Text = ef.BlurRadius.ToString();
+                DropShadowDepth.Text = ef.ShadowDepth.ToString();
+                DropShadowColor.Text = ef.Color.ToString();
+            }
+            if (TextOptions.GetTextFormattingMode(target) == TextFormattingMode.Ideal)
+            {
+                textFormattingMode.SelectedIndex = 0;
+            }
+            else
+            {
+                textFormattingMode.SelectedIndex = 1;
+            }
         }
     }
 }
