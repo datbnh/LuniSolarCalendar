887f0e55df738169102a65ac935edb0fe905ea45
datbnh <datbnh@gmail.com>
2017-12-15 22:22:17 +1100

    + Add outmost configuration class.

---
+++ VietnameseCalendarUI/Configuration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Augustine.VietnameseCalendar.UI
+{
+    [DataContract]
+    public class Configuration
+    {
+        [DataMember]
+        public Theme Theme;
+
+        [DataMember]
+        public Location Location;
+
+        [DataMember]
+        public CalendarType CalendarType;
+
+        [DataMember]
+        public ViewMode ViewMode;
+
+        public static Configuration DefaultConfiguration
+        {
+            get => new Configuration()
+            {
+                Theme = Themes.LightSemiTransparent,
+                CalendarType = CalendarType.Month,
+                ViewMode = ViewMode.Widget,
+            };
+        }
+    }
+
+
+    [DataContract]
+    public class Location
+    {
+        [DataMember]
+        public double Top;
+        [DataMember]
+        public double Left;
+        [DataMember]
+        public double Width;
+        [DataMember]
+        public double Height;
+    }
+
+    [DataContract]
+    public enum CalendarType : byte
+    {
+        [EnumMember]
+        Day = 0x00,
+        [EnumMember]
+        Month = 0x01,
+    }
+
+    [DataContract]
+    public enum ViewMode : byte
+    {
+        [EnumMember]
+        Widget = 0x00,
+        [EnumMember]
+        Normal = 0x01,
+    }
+}
